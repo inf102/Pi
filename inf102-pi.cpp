28be57ea70da216b0b0010532441402169752aea
inf102 <156289698+inf102@users.noreply.github.com>
2026-03-04 09:48:32 +0900

    Refactor Monte Carlo pi calculation implementation

--- inf102-pi.cpp
+++ inf102-pi.cpp
@@ -1,32 +1,30 @@
-////////////////////////////////
-// pi c++ Ver
-// inf102 S.H
-////////////////////////////////
+////////////////////////////////////////////////
+// MonteCarlomethod for calculating pi c++ Ver.
+// single thread type.
+// (c)2025 inf102 S.H.
+///////////////////////////////////////////////
 
 #include <iostream>
 #include <time.h>
 #include <random>
 #include <chrono>
 #include <windows.h>
-
-using namespace std;
+#include <conio.h>
 
 int main() {
-
     double X, Y;
     long long c = 0;
 
     std::random_device rd;
     std::default_random_engine eng(rd());
-    std::uniform_real_distribution<double> distr(0.1, 1.0);
+    std::uniform_real_distribution<double> distr(0.0, 1.0);
 
     SYSTEMTIME lt;
-
     double long g = 0;
     long long f;
     long long n;
 
-    cout << "Monte Carlo method for calculating pi. (c)inf102 2025.\r\nloop 1,000,000,000\r\n\r\n";
+    std::cout << "\r\nMonte Carlo method for calculating pi. (c)2025 inf102 S.HIRAYANAGI.\r\nsingle thread. loop 1,000,000,000\r\n\r\n";
 
     f = 1000000000L;
     GetLocalTime(&lt);
@@ -37,18 +35,16 @@ int main() {
         Y = distr(eng);
 
         if ((X * X + Y * Y) < 1.0) c++;
-            // g= (4.0 * c / n);
-            // std::cout << n <<" " << g << "\n";
-        }
+        // g= (4.0 * c / n);
+        // std::cout << n <<" " << g << "\n";
+    }
 
-        GetLocalTime(&lt);
-        printf("End   %02d:%02d:%2d", lt.wHour, lt.wMinute, lt.wSecond);
+    GetLocalTime(&lt);
+    printf("End   %02d:%02d:%2d", lt.wHour, lt.wMinute, lt.wSecond);
+    g = (4.0 * c / f);
+    std::cout << "\nabout Pi " << g << "\n\n";
 
-        g = (4.0 * c / f);
-        std::cout << "\nabout Pi " << g << "\n\n";
-
-
-    int a;
-    std::cin >> a;
-
+    std::cout << "Press any key.";
+    char ch = _getch();
 }
+
